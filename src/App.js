3755df1edf1c888642d932ea8fec74bfbd8d61ce
Pavan Chitradurga Thammanna <mail@pavanct.com>
2020-10-19 04:22:46 +0200

    add spotify api,  use context api, reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React, { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 import "./App.css";
 import Login from "./Login";
+import Player from "./Player";
 import { getTokenFromUrl } from "./spotify";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
-    const token = hash.access_token;
-    if (token) {
-      setToken(token);
+    const _token = hash.access_token;
+    if (_token) {
+      // setToken(_token);
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      })
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
     }
-
-    console.log("access_token", token);
   }, []);
+  console.log("token", token);
+  console.log("user", user);
 
-  return (
-    <div className="App">{token ? <h1>I am logged in</h1> : <Login />}</div>
-  );
+
+  return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
