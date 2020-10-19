fdda55088eb28caa93b7f189521777206590e61f
Pavan Chitradurga Thammanna <mail@pavanct.com>
2020-10-19 03:15:45 +0200

    get token and store state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import './App.css';
-import Login from './Login';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const token = hash.access_token;
+    if (token) {
+      setToken(token);
+    }
+
+    console.log("access_token", token);
+  }, []);
+
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <div className="App">{token ? <h1>I am logged in</h1> : <Login />}</div>
   );
 }
 
 export default App;
-
