6a26d96902024c8f44b1e39da51390d360c65c64
Pavan Chitradurga Thammanna <mail@pavanct.com>
2020-10-19 05:14:02 +0200

    construct sidebar

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import './SideBar.css'
+import SideBarOption from './SideBarOption'
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+
 
 function SideBar() {
     return (
       <div className="sidebar">
         <img
-         className="sidebar__logo"
+          className="sidebar__logo"
           src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
           alt=""
-          srcset=""
         />
+        <SideBarOption title="Home" Icon={HomeIcon} />
+        <SideBarOption title="Search" Icon={SearchIcon} />
+        <SideBarOption title="Your Library" Icon={LibraryMusicIcon} />
+        <br/>
+        <strong className="sidebar__title">PLAYLISTS</strong>
+        <hr/>
       </div>
     );
 }
